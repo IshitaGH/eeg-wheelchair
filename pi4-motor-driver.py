523c915c95a0567ba53c339056e5b88025821225
IshitaGH <ishitagh16@gmail.com>
2025-05-06 23:52:14 -0700

    Ramp up pwm to reduce sheer force when changing direction + refactor

--- pi4-motor-driver.py
+++ pi4-motor-driver.py
@@ -1,5 +1,4 @@
 import pigpio
-import keyboard
 import time
 from enum import Enum
 
@@ -13,77 +12,76 @@ INA2 = 22
 INB2 = 23
 PWM2 = 13
 
+
 class Direction(Enum):
-    FORWARD = 1
-    BACKWARD = 2
-    LEFT = 3
-    RIGHT = 4
-    STOP = 5
-
-# TODO: calibrate these values for the wheelchair
-class Speed(Enum):
-    FORWARD_SPEED = 80
-    BACKWARD_SPEED = 80
-    RIGHT_SPEED = 80
-    LEFT_SPEED = 80
+    FORWARD = "w"
+    BACKWARD = "s"
+    LEFT = "a"
+    RIGHT = "d"
+    STOP = " "
 
 
-def drive_motor(direction):
-    if direction == Direction.FORWARD.value:
-        pi.write(INA1, 1)
-        pi.write(INB1, 0)
-        pi.write(INA2, 1)
-        pi.write(INB2, 0)
-        pi.set_PWM_dutycycle(PWM1, abs(Speed.FORWARD_SPEED.value))
-        pi.set_PWM_dutycycle(PWM2, abs(Speed.FORWARD_SPEED.value))
-    elif direction == Direction.BACKWARD.value:
-        pi.write(INA1, 0)
-        pi.write(INB1, 1)
-        pi.write(INA2, 0)
-        pi.write(INB2, 1)
-        pi.set_PWM_dutycycle(PWM1, abs(Speed.BACKWARD_SPEED.value))
-        pi.set_PWM_dutycycle(PWM2, abs(Speed.BACKWARD_SPEED.value))
-    elif direction == Direction.RIGHT.value:
-        pi.write(INA1, 1)
-        pi.write(INB1, 0)
-        pi.write(INA2, 0)
-        pi.write(INB2, 1)
-        pi.set_PWM_dutycycle(PWM1, abs(Speed.RIGHT_SPEED.value))
-        pi.set_PWM_dutycycle(PWM2, abs(Speed.RIGHT_SPEED.value))
-    elif direction == Direction.LEFT.value:
-        pi.write(INA1, 0)
-        pi.write(INB1, 1)
-        pi.write(INA2, 1)
-        pi.write(INB2, 0)
-        pi.set_PWM_dutycycle(PWM1, abs(Speed.LEFT_SPEED.value))
-        pi.set_PWM_dutycycle(PWM2, abs(Speed.LEFT_SPEED.value))
-    elif direction == Direction.STOP.value:
-        pi.write(INA1, 0)
-        pi.write(INB1, 0)
-        pi.write(INA2, 0)
-        pi.write(INB2, 0)
-        pi.set_PWM_dutycycle(PWM1, 0)
-        pi.set_PWM_dutycycle(PWM2, 0)
-    
+# calibrate these values based on how fast you want to go
+# range is 0-255
+DIRECTION_SPEED = {
+    Direction.FORWARD.value: 70,
+    Direction.BACKWARD.value: 55,
+    Direction.RIGHT.value: 40,
+    Direction.LEFT.value: 40,
+}
 
-if __name__ == '__main__':
+DIRECTION_IN = {
+    Direction.FORWARD.value: [1, 0, 0, 1],
+    Direction.BACKWARD.value: [0, 1, 1, 0],
+    Direction.RIGHT.value: [0, 1, 0, 1],
+    Direction.LEFT.value: [1, 0, 1, 0],
+    Direction.STOP.value: [0, 0, 0, 0],
+}
+
+# controls how fast the wheelchair changes directions
+# I wouldn't make it much faster than this unless you
+# end up machining the aluminum keys because the sheer
+# force will break the plastic keys
+TIME_DELAY = 0.020
+
+
+def drive_motors(dir):
+    global curr_direction
+    if curr_direction == dir:
+        return
+
+    for IN, d in zip([INA1, INB1, INA2, INB2], DIRECTION_IN.get(dir)):
+        pi.write(IN, d)
+
+    if dir == Direction.STOP.value:
+        for i in range(DIRECTION_SPEED.get(curr_direction), 0, -1):
+            pi.set_PWM_dutycycle(PWM1, i)
+            pi.set_PWM_dutycycle(PWM2, i)
+            time.sleep(TIME_DELAY)
+    else:
+        curr_direction = dir
+        drive_motors(Direction.STOP.value)
+
+        for i in range(DIRECTION_SPEED.get(dir)):
+            pi.set_PWM_dutycycle(PWM1, i)
+            pi.set_PWM_dutycycle(PWM2, i)
+            time.sleep(TIME_DELAY)
+
+    curr_direction = dir
+
+
+if __name__ == "__main__":
     for pin in [INA1, INB1, INA2, INB2, PWM1, PWM2]:
         pi.set_mode(pin, pigpio.OUTPUT)
 
+    global curr_direction
+    curr_direction = None
     try:
         while True:
-            val = input("Enter your value: ")
-            if val == 'w':
-                drive_motor(Direction.FORWARD.value)
-            elif val == 's':
-                drive_motor(Direction.BACKWARD.value)
-            elif val == 'a':
-                drive_motor(Direction.LEFT.value)
-            elif val == 'd':
-                drive_motor(Direction.RIGHT.value)
-            elif keyboard.is_pressed('space'):
-                drive_motor(Direction.STOP.value)
+            val = input("Enter your direction: ")
+            if val in Direction._value2member_map_:
+                drive_motors(val)
     except KeyboardInterrupt:
         print("Stopping")
-        drive_motor(Direction.STOP.value)
+        drive_motors(Direction.STOP.value)
         pi.stop()
